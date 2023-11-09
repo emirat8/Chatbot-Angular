af5b69917fbc0433ace9689396c5f40a499a7147
Emirat Millenium Try <emirat.millenium@yahoo.co.id>
2023-11-09 22:08:05 +0700

    Update bubble chat

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ChatService } from '../chat.service';
 import { HttpClient } from '@angular/common/http';
-import { ViewChild, ViewContainerRef, ComponentFactoryResolver } from '@angular/core';
+import { ViewChild, ViewContainerRef, ComponentFactoryResolver, ElementRef, AfterViewChecked } from '@angular/core';
 import { RequestComponent } from '../request/request.component';
 import { ResponseComponent } from '../response/response.component';
 
@@ -14,47 +14,54 @@ export class ChatComponent {
   question=""
   response:any
 
+  @ViewChild('scrollableDiv', { static: false }) private scrollableDiv!: ElementRef;
+
   @ViewChild('dynamicBubble', { read: ViewContainerRef }) dynamicBubble: ViewContainerRef | null = null;
   
   constructor(private chatService : ChatService, private http:HttpClient, private componentFactoryResolver: ComponentFactoryResolver){}
 
-  askQuestion() {
+
+  askQuestionEnter(e:any){
+    e.preventDefault()
+    this.askQuestion()
+  }
+
+  askQuestion() {  
+    const questionTemp = this.question;
+    this.question = "";
+    
     if (this.dynamicBubble) {
       const factory = this.componentFactoryResolver.resolveComponentFactory(RequestComponent);
       const componentRef = factory.create(this.dynamicBubble.injector);
 
       const requestComponent = componentRef.instance as RequestComponent;
-      requestComponent.request = this.question;
+      requestComponent.request = questionTemp;
 
       this.dynamicBubble.insert(componentRef.hostView);
     }
 
-    this.chatService.post(this.question).subscribe((
+    this.chatService.post(questionTemp).subscribe((
       resp: any) => {
-        console.log(resp.response);
+        console.log(resp);
 
         if (this.dynamicBubble) {
           const factory = this.componentFactoryResolver.resolveComponentFactory(ResponseComponent);
           const componentRef = factory.create(this.dynamicBubble.injector);
     
           const responseComponent = componentRef.instance as ResponseComponent;
-          responseComponent.response = resp.response;
+          responseComponent.response = resp;
     
           this.dynamicBubble.insert(componentRef.hostView);
         }
       }
     )
+
+    this.scrollToBottom();
   }
 
-  addComponent() {
-    if (this.dynamicBubble) {
-      const factory = this.componentFactoryResolver.resolveComponentFactory(RequestComponent);
-      const componentRef = factory.create(this.dynamicBubble.injector);
-
-      const requestComponent = componentRef.instance as RequestComponent;
-      requestComponent.request = this.question;
-
-      this.dynamicBubble.insert(componentRef.hostView);
-    }
+  scrollToBottom(): void {
+    try {
+      this.scrollableDiv.nativeElement.scrollTop = this.scrollableDiv.nativeElement.scrollHeight;
+    } catch(err) { }
   }
 }
