81be59141d89f3ad76b30bd798d7f18b5da26956
Emirat Millenium Try <emirat.millenium@yahoo.co.id>
2023-11-11 23:57:40 +0700

    Update UI

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -5,6 +5,7 @@ import { ViewChild, ViewContainerRef, ComponentFactoryResolver, ElementRef} from
 import { RequestComponent } from '../request/request.component';
 import { ResponseComponent } from '../response/response.component';
 import { LoadingService } from '../loading.service';
+import { ModalDismissReasons, NgbDatepickerModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-chat',
@@ -12,12 +13,79 @@ import { LoadingService } from '../loading.service';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent {
+  closeResult = '';
   question=""
   response:any
+  errorMessage=""
   isWaitingForResponse = false;
   shouldAutoScroll = false;
 
   @ViewChild('scrollableDiv') private scrollContainer!: ElementRef;
+  @ViewChild('errorModal') errorModal: ElementRef | undefined;
+  @ViewChild('dynamicBubble', { read: ViewContainerRef }) dynamicBubble: ViewContainerRef | null = null;
+  
+  constructor(private modalService: NgbModal, private loadingService: LoadingService, private chatService : ChatService, private http:HttpClient, private componentFactoryResolver: ComponentFactoryResolver){}
+
+  askQuestionEnter(e:any){
+    e.preventDefault()
+    this.askQuestion()
+  }
+
+  askQuestion() {
+    if (this.question == "") {
+      this.open(this.errorModal, "Pertanyaan tidak boleh kosong")
+    } else {
+      this.loadingService.show();
+      const questionTemp = this.question;
+      this.question = "";
+      this.isWaitingForResponse = true;
+      
+      if (this.dynamicBubble) {
+        const factory = this.componentFactoryResolver.resolveComponentFactory(RequestComponent);
+        const componentRef = factory.create(this.dynamicBubble.injector);
+  
+        const requestComponent = componentRef.instance as RequestComponent;
+        requestComponent.request = questionTemp;
+  
+        this.dynamicBubble.insert(componentRef.hostView);
+        setTimeout(() => {
+          this.autoScroll();
+        }, 500);
+      }
+
+      this.chatService.post(questionTemp).subscribe((
+        resp: any) => {
+          if (resp.status === 200) {
+            if (this.dynamicBubble) {
+              const factory = this.componentFactoryResolver.resolveComponentFactory(ResponseComponent);
+              const componentRef = factory.create(this.dynamicBubble.injector);
+        
+              const responseComponent = componentRef.instance as ResponseComponent;
+              
+              responseComponent.response = resp.text;
+    
+              this.loadingService.hide();
+              this.dynamicBubble.insert(componentRef.hostView);
+              this.isWaitingForResponse = false;
+              setTimeout(() => {
+                this.autoScroll();
+              }, 500);
+            }
+          } else {
+            this.loadingService.hide();
+            this.isWaitingForResponse = false;
+            this.open(this.errorModal, resp.text);
+          }
+        },
+        error => {
+          this.loadingService.hide();
+          this.isWaitingForResponse = false;
+          // Tangani error jika terjadi kesalahan jaringan, dll.
+          this.open(this.errorModal, "Gagal mendapatkan respons: " + error.message);
+        }
+      )
+    }
+  }
 
   onModelChange(textArea: HTMLTextAreaElement) {
     if (this.isWaitingForResponse == false) {
@@ -29,52 +97,8 @@ export class ChatComponent {
     window.scrollTo({top: this.scrollContainer.nativeElement.scrollHeight, behavior: 'smooth'});
   }
 
-  @ViewChild('dynamicBubble', { read: ViewContainerRef }) dynamicBubble: ViewContainerRef | null = null;
-  
-  constructor(private loadingService: LoadingService, private chatService : ChatService, private http:HttpClient, private componentFactoryResolver: ComponentFactoryResolver){}
-
-  askQuestionEnter(e:any){
-    e.preventDefault()
-    this.askQuestion()
-  }
-
-  askQuestion() {
-    this.loadingService.show();
-    const questionTemp = this.question;
-    this.question = "";
-    this.isWaitingForResponse = true;
-    
-    if (this.dynamicBubble) {
-      const factory = this.componentFactoryResolver.resolveComponentFactory(RequestComponent);
-      const componentRef = factory.create(this.dynamicBubble.injector);
-
-      const requestComponent = componentRef.instance as RequestComponent;
-      requestComponent.request = questionTemp;
-
-      this.dynamicBubble.insert(componentRef.hostView);
-      setTimeout(() => {
-        this.autoScroll();
-      }, 500);
-    }
-
-    this.chatService.post(questionTemp).subscribe((
-      resp: any) => {
-        if (this.dynamicBubble) {
-          const factory = this.componentFactoryResolver.resolveComponentFactory(ResponseComponent);
-          const componentRef = factory.create(this.dynamicBubble.injector);
-    
-          const responseComponent = componentRef.instance as ResponseComponent;
-          
-          responseComponent.response = resp.text;
-
-          this.loadingService.hide();
-          this.dynamicBubble.insert(componentRef.hostView);
-          this.isWaitingForResponse = false;
-          setTimeout(() => {
-            this.autoScroll();
-          }, 500);
-        }
-      }
-    )
+  open(content: any, message: string) {
+    this.errorMessage = message
+    this.modalService.open(content)
   }
 }
