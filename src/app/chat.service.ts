43b72c38372fe430f862e1983a821d942db3eaa6
Emirat Millenium Try <emirat.millenium@yahoo.co.id>
2023-11-13 17:25:31 +0700

    update method

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -7,12 +7,20 @@ import { Observable } from 'rxjs';
 })
 export class ChatService {
 
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer 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',
+    'OpenAI-Beta': 'assistants=v1',
+  });
+
   constructor(private http: HttpClient) { }
 
-  post(obj: any): Observable<any> {
+  post(query_text: string, thread_id: string): Observable<any> {
     const formData = new FormData();
-    formData.append('query_text', obj); // Ganti dengan nama field yang sesuai
-    console.log(obj)
+    formData.append('query_text', query_text);
+    formData.append('thread_id', thread_id); // Menambahkan parameter thread_id
+    console.log(query_text, thread_id); // Menampilkan kedua parameter di konsol
+
     return this.http.post<any>('http://localhost:5601/query', formData);
-  }
+}
 }
